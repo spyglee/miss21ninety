9d45ef6da6c5c4240f76bf67a7d3ec0b64f1230f
Maksym <m.bahrov@gmail.com>
2025-01-12 17:40:30 +0100

    added expert section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AboutSection from './components/homepage/AboutSection'
 import FoodSection from './components/homepage/FoodSection'
 import StartSection from './components/homepage/StartSection'
 import SeparateSection from './components/homepage/SeparateSection'
+import ExpertsSection from './components/homepage/ExpertsSection'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <FoodSection />
         <HeroSection />
         <SeparateSection />
+        <ExpertsSection />
       </main>
     </>
   )
